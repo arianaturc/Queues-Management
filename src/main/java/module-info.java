184682a8a25cf317398f4b9c78f1c465e4f6c0dc
arianaturc <ariana.turc@yahoo.com>
2025-03-27 11:38:07 +0200

    prob

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.pt2025_30422_turc_ariana_assignment_2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.pt2025_30422_turc_ariana_assignment_2 to javafx.fxml;
+    exports org.example.pt2025_30422_turc_ariana_assignment_2;
+}
